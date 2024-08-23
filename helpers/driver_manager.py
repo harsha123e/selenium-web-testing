90cbfb1503d6abb18c0e7007779d20c4af52957b
Harsha <nsriharsha3@gmail.com>
2024-08-23 14:12:43 +0530

    Headless Browser only on CI/CD

--- helpers/driver_manager.py
+++ helpers/driver_manager.py
@@ -3,10 +3,15 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+import os
 
 def get_driver() -> WebDriver:
     options = ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless')  # Optional: Run Chrome in headless mode
+
+    # Check if running in CI/CD environment
+    if os.getenv('CI') == 'true':
+        options.add_argument('--headless')  # Run Chrome in headless mode in CI/CD
+
     return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
